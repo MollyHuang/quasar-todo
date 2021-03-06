2460f789af1312b344d4156b824c7a833294dd94
Molly Huang <molly.h@gmail.com>
2021-03-06 22:40:39 +0800

    #4-25:07-Add Todo List with checkbox, label, delete icons

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,11 +1,77 @@
 <template>
-  <q-page>
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+    
+    <q-list 
+      class="bg-white"
+      separator
+      bordered >
+
+      <q-item 
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        clickable
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1' : task.done }"
+        v-ripple>
+
+        <q-item-section avatar>
+          <q-checkbox 
+            v-model="task.done" 
+            class="no-pointer-events"
+            color="primary" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section
+          v-if="task.done"
+          side>
+          <q-btn @click.stop="deleteTask(index)" flat round color="primary" icon="delete" dense />
+        </q-item-section>
+
+      </q-item>
+
+    </q-list>
+
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'Get bananas',
+          done: true
+        },
+        {
+          title: 'Eat bananas',
+          done: false
+        },
+        {
+          title: 'Poo bananas',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1)
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+
+</style>
