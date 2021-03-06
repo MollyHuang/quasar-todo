a72fc8a1aa59677bc4aac8c90df8ac02c77a4ac6
Molly Huang <molly.h@gmail.com>
2021-03-06 21:51:15 +0800

    #3-16:40-Update left sidebar, add todo page & help page with router

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,12 +10,6 @@
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
       <div class="q-px-lg q-pt-xl q-mb-md">
         <div class="text-h3">Todo</div>
@@ -27,86 +21,70 @@
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
-      content-class="bg-grey-1"
+      :width="250"
+      :breakpoint="600"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+        <q-list padding>
+
+          <q-item
+            to="/"
+            exact
+            clickable 
+            v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section active clickable v-ripple>
+              Todo
+            </q-item-section>
+          </q-item>
+
+          <q-item 
+            to="/help"
+            exact
+            clickable 
+            v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>
+              Help
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </q-scroll-area>
+
+      <q-img class="absolute-top" src="~assets/lake.jpg" style="height: 192px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://avatars.githubusercontent.com/u/6235434?s=60&v=4">
+          </q-avatar>
+          <div class="text-weight-bold">Molly Huang</div>
+          <div>@MollyHuang</div>
+        </div>
+      </q-img>
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
 import { date } from 'quasar'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      leftDrawerOpen: false
     }
   },
   computed: {
---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-noe">Help</h5>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium molestiae aperiam dicta accusamus expedita neque nostrum placeat iste totam vitae amet assumenda alias rem est, doloremque nulla minima voluptatibus necessitatibus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus itaque blanditiis reprehenderit eaque saepe tenetur illo voluptas cumque, fuga officia tempore dolorem voluptate neque explicabo, dolorum nulla minima quaerat ut?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
